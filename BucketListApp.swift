990d114c167c828972f2e1a4c3c16d33474207f5
LikoSetiawan <128354782+LikoSetiawan@users.noreply.github.com>
2024-04-16 22:53:13 +0800

    Initial commit

---
+++ BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Liko Setiawan on 16/04/24.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
